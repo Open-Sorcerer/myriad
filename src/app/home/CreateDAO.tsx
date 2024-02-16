bf25d0013518a0fc00f03dd11fac19c89c283553
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-02-17 00:46:43 +0530

    fix supabase db

--- src/app/home/CreateDAO.tsx
+++ src/app/home/CreateDAO.tsx
@@ -1,6 +1,5 @@
-import { useStore } from '@store'
-import React, { useState } from 'react'
-import { createDAO } from '@components/createDAO'
+import {useStore} from '@store'
+import React, {useState} from 'react'
 import {
 	Drawer,
 	DrawerClose,
@@ -11,101 +10,109 @@ import {
 	DrawerTitle,
 	DrawerTrigger,
 } from '@components/ui/drawer'
+import client from "@lib/db";
 
 const CreateDAO = () => {
-	const [daoName, setDaoName] = useState('')
-	const [daoDescription, setDaoDescription] = useState('')
-	const [isImageUploading, setIsImageUploading] = useState(false)
-	const [daoImage, setDaoImage] = useState('')
-	const { identity } = useStore()
+    const [daoName, setDaoName] = useState('')
+    const [daoDescription, setDaoDescription] = useState('')
+    const [isImageUploading, setIsImageUploading] = useState(false)
+    const [daoImage, setDaoImage] = useState('')
+    const {identity} = useStore()
 
-	const createDao = async () => {
-		await createDAO(daoName, daoDescription, daoImage)
-	}
-
-	return (
-		<Drawer>
-			<DrawerTrigger asChild>
-				<button
-					type="button"
-					className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-[#1B1F3B] shadow-sm text-white hover:bg-black  disabled:opacity-50 disabled:pointer-events-none"
-				>
-					Create DAO
-				</button>
-			</DrawerTrigger>
-			<DrawerContent>
-				<div className="mx-auto w-full max-w-sm">
-					<DrawerHeader>
-						<DrawerTitle>Create DAO</DrawerTitle>
-						<DrawerDescription>Fabricate your brand-new DAO</DrawerDescription>
-					</DrawerHeader>
-					<div className="p-4 pb-0">
-						<form className="w-full flex flex-col justify-center items-start gap-5">
-							<input
-								type="text"
-								className="w-full py-3 px-4 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-								placeholder="DAO Name"
-								onChange={(e: any) => setDaoName(e.target.value)}
-								value={daoName}
-							/>
-							<textarea
-								className="w-full py-3 px-4 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-								placeholder="DAO Description"
-								rows={4}
-								onChange={(e: any) => setDaoDescription(e.target.value)}
-								value={daoDescription}
-							/>
-							<label htmlFor="file-input" className="sr-only">
-								Choose file
-							</label>
-							<input
-								type="file"
-								name="file-input"
-								id="file-input"
-								className="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
+    return (
+        <Drawer>
+            <DrawerTrigger asChild>
+                <button
+                    type="button"
+                    className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-[#1B1F3B] shadow-sm text-white hover:bg-black  disabled:opacity-50 disabled:pointer-events-none"
+                >
+                    Create DAO
+                </button>
+            </DrawerTrigger>
+            <DrawerContent>
+                <div className="mx-auto w-full max-w-sm">
+                    <DrawerHeader>
+                        <DrawerTitle>Create DAO</DrawerTitle>
+                        <DrawerDescription>Fabricate your brand-new DAO</DrawerDescription>
+                    </DrawerHeader>
+                    <div className="p-4 pb-0">
+                        <form className="w-full flex flex-col justify-center items-start gap-5">
+                            <input
+                                type="text"
+                                className="w-full py-3 px-4 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                                placeholder="DAO Name"
+                                onChange={(e: any) => setDaoName(e.target.value)}
+                                value={daoName}
+                            />
+                            <textarea
+                                className="w-full py-3 px-4 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                                placeholder="DAO Description"
+                                rows={4}
+                                onChange={(e: any) => setDaoDescription(e.target.value)}
+                                value={daoDescription}
+                            />
+                            <label htmlFor="file-input" className="sr-only">
+                                Choose file
+                            </label>
+                            <input
+                                type="file"
+                                name="file-input"
+                                id="file-input"
+                                className="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
                                     file:bg-gray-100 file:border-0
                                     file:me-4
                                     file:py-4 file:px-4"
-								onChange={(e: any) => {
-									setIsImageUploading(true)
-									const formData = new FormData()
-									formData.append('image', e.target.files[0])
-									fetch('/api/uploadFile', {
-										method: 'POST',
-										body: formData,
-									}).then(async (res: any) => {
-										const { protocolLink } = await res.json()
-										console.log(protocolLink)
-										setDaoImage(`${protocolLink}/DAO_Image.png`)
-										setIsImageUploading(false)
-									})
-								}}
-							/>
-						</form>
-					</div>
-					<DrawerFooter>
-						<button
-							type="button"
-							onClick={async () => {
-								await createDao()
-							}}
-							className="py-3 px-4 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-[#1B1F3B] shadow-sm text-white hover:bg-black  disabled:opacity-50 disabled:pointer-events-none"
-						>
-							{isImageUploading ? 'Uploading Image...' : 'Create DAO'}
-						</button>
-						<DrawerClose asChild>
-							<button
-								type="button"
-								className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
-							>
-								Cancel
-							</button>
-						</DrawerClose>
-					</DrawerFooter>
-				</div>
-			</DrawerContent>
-		</Drawer>
-	)
+                                onChange={(e: any) => {
+                                    setIsImageUploading(true)
+                                    const formData = new FormData()
+                                    formData.append('image', e.target.files[0])
+                                    fetch('/api/uploadFile', {
+                                        method: 'POST',
+                                        body: formData,
+                                    }).then(async (res: any) => {
+                                        const {protocolLink} = await res.json()
+                                        console.log(protocolLink)
+                                        setDaoImage(`${protocolLink}/DAO_Image.png`)
+                                        setIsImageUploading(false)
+                                    })
+                                }}
+                            />
+                        </form>
+                    </div>
+                    <DrawerFooter>
+                        <button
+                            type="button"
+                            onClick={() => createDAODBHandler(daoName, daoDescription, daoImage)}
+                            className="py-3 px-4 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-[#1B1F3B] shadow-sm text-white hover:bg-black  disabled:opacity-50 disabled:pointer-events-none"
+                        >
+                            {isImageUploading ? 'Uploading Image...' : 'Create DAO'}
+                        </button>
+                        <DrawerClose asChild>
+                            <button
+                                type="button"
+                                className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
+                            >
+                                Cancel
+                            </button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                </div>
+            </DrawerContent>
+        </Drawer>
+    )
 }
+
+export const createDAODBHandler = async (name: string, description: string, image: string) => {
+    const {error, data} = await client.from('Test').insert({
+        id: 2,
+        name: 'New',
+    })
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(data)
+    }
+}
+
 
 export default CreateDAO
